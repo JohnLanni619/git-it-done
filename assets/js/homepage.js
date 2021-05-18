47ff9ac46283e6f3b1d3059507c4396729eef76d
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-05-18 13:09:44 -0400

    6.1 snapshot

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,16 +1,14 @@
 var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
     });
+  });
 };
 
-var response = fetch("https://api.github.com/users/octocat/repos");
-console.log(response);
-
-getUserRepos("johnlanni619");
+getUserRepos("lernantino");
